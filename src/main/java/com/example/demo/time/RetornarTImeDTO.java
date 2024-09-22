49b3c6fa57443d70c1b96cf0a3d7581715153784
Thiago Penha <thiagoppc@al.insper.edu.br>
2024-09-22 18:16:43 -0300

    pls z

---
+++ src/main/java/com/example/demo/time/RetornarTImeDTO.java
@@ -0,0 +1,46 @@
+package com.example.jogadores.time;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+public class RetornarTImeDTO {
+
+    private String identificador;
+    private String estadio;
+    private String estado;
+    private String nome;
+
+    public String getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(String identificador) {
+        this.identificador = identificador;
+    }
+
+    public String getEstadio() {
+        return estadio;
+    }
+
+    public void setEstadio(String estadio) {
+        this.estadio = estadio;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
